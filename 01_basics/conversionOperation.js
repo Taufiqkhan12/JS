768b813f38a32f6cdd078b99b6c35b7325018581
Taufiqkhan12 <ktaufiq913@gmail.com>
2024-08-06 12:49:49 +0530

    conversion operation

---
+++ 01_basics/conversionOperation.js
@@ -0,0 +1,43 @@
+let score = "33abc";
+
+// console.log(typeof score);
+
+let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// "null" => NaN/0
+// "undefined" => NaN
+
+let isLoggedIn = "0";
+
+let isLoggedInBoolean = Boolean(isLoggedIn);
+// console.log(isLoggedInBoolean);
+// console.log(typeof isLoggedInBoolean);
+
+// true => 1
+// false => 0
+// "" => false
+// "taufiq" => true
+
+// *************************Operations***********************************
+
+let value = 3;
+let negValue = -value;
+// console.log(negValue);
+
+let str1 = "1";
+let str2 = "2";
+let str3 = str1 + str2;
+// console.log(str3);
+
+// console.log(1 + 2);
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+console.log(+true);
+console.log(+"");
