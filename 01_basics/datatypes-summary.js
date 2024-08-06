2a9edd51ab2956153c0e8f639c67b669d0363c61
Taufiqkhan12 <ktaufiq913@gmail.com>
2024-08-06 15:57:28 +0530

    stack & heap memory

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -5,3 +5,25 @@
 // Reference (Non-Primitive)
 
 // Array, Object, Function
+
+// ************************ Memory Types *******************************
+
+// Stack (Primitive), Heap (Non-Primitive)
+
+let myName = "Taufiq";
+let anotherName = myName;
+anotherName = " Khan";
+
+console.log(myName);
+console.log(anotherName);
+
+let userOne = {
+  email: "taufiq@gmail.com",
+  upi: "user@ybl",
+};
+
+let userTwo = userOne;
+userTwo.email = "khan@.com";
+
+console.log(userOne);
+console.log(userTwo);
