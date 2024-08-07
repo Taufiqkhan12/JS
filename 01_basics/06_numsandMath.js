319a3222c9ff198b8c17468515596de7d724dc22
Taufiqkhan12 <ktaufiq913@gmail.com>
2024-08-07 12:36:27 +0530

    Math and Num function

---
+++ 01_basics/06_numsandMath.js
@@ -0,0 +1,33 @@
+const score = 400;
+// console.log(score);
+
+const balance = new Number(100);
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+const otherNumber = 123.856;
+// console.log(otherNumber.toPrecision(4));
+
+const hunderds = 1000000;
+// console.log(hunderds.toLocaleString("en-IN"));
+
+// ************************ Maths ****************************
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.3));
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.3));
+// console.log(Math.floor(4.3));
+// console.log(Math.sqrt(5));
+// console.log(Math.min(34, 6, 78, 43, 78));
+
+console.log(Math.random());
+console.log(Math.floor(Math.random() * 10) + 1);
+
+const min = 10;
+const max = 20;
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
