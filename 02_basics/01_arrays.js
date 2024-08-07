41fd89065fc3093f63c8239ad4e403d7c4136717
Taufiqkhan12 <ktaufiq913@gmail.com>
2024-08-07 20:43:35 +0530

    arrays

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,34 @@
+// Array
+const myArr = [0, 1, 2, 3, 4];
+const myHeros = ["name1", "name2"];
+
+const newArr = new Array(1, 2, 3, 4);
+// console.log(myArr[0]);
+
+//Array Method
+
+// myArr.push(5, 6);
+// myArr.pop();
+
+// myArr.unshift(9);
+// myArr.shift();
+
+// console.log(myArr.includes(9));
+// console.log(myArr.indexOf(9));
+// console.log(myArr.indexOf(3));
+
+const newArr2 = myArr.join();
+
+// console.log(myArr);
+// console.log(newArr2);
+
+// console.log("A", myArr);
+
+const myNewArr = myArr.slice(1, 3);
+// console.log(myNewArr);
+
+// console.log("B", myArr);
+
+const myNewArr1 = myArr.splice(1, 3);
+// console.log("C", myArr);
+// console.log(myNewArr1);
